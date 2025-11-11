4dd022cde760e006f8bcfc456804ce2248815214
Jotave <jguedesscz04@outlook.com>
2025-11-11 18:26:20 -0300

    Fix Wishlist routes

--- Backend/Controller/wishlistController.js
+++ Backend/Controller/wishlistController.js
@@ -1,4 +1,4 @@
-import WishList from "../Model/wishlist.js";
+import Wishlist from "../Model/wishlist.js";
 
 
 /*
@@ -6,25 +6,28 @@ Olá, pfv corrigir certas coisas, nao sei ao certo se tem id no client ou produc
 faça a mudança, alguns erros podem ocorrer devidos a nomes errados, tentarei fazer o maximo para nao por nomes incorretos :)
 */
 export const getWishlist = async (req, res) => {
-    try {
-        const w = await WishList.findOne({client: req.params.clientId }).populate("products", "name price photo");
+  try {
+    const wishlist = await Wishlist.findOne({ client: req.params.clientId })
+      .populate("products", "name price photo");
 
-        if(!w)
-            return res.status(404).json({message: "Lista de Desejos Vazia"})
-        res.json(w);
-    } catch (error) {
-        res.status(500).json({ message: "Erro ao buscar wishlist",error});
-    }
+    if (!wishlist)
+      return res.status(404).json({ message: "Lista de Desejos Vazia" });
+
+    res.status(200).json(wishlist);
+  } catch (error) {
+    console.error("Erro ao buscar wishlist:", error);
+    res.status(500).json({ message: "Erro ao buscar wishlist", error });
+  }
 };
 
 export const addProductToWishlist = async(req,res) => {
     try {
-        const { clientId, productId} = req.body;
+        const { clientId, productId } = req.body;
 
-        let w = await WishList.findOne({ client: clientId});
+        let w = await Wishlist.findOne({ client: clientId});
 
         if(!w){
-            w = new WishList({client: clientId, products: [productId]});
+            w = new Wishlist({client: clientId, products: [productId]});
         } else {
             if(!w.products.includes(productId)) {
                 w.products.push(productId);
@@ -42,7 +45,7 @@ export const removeProductFromWishlist = async (req, res) => {
     try {
         const { clientId, productId } = req.body;
 
-        const w = await WishList.findOne({ client: clientId});
+        const w = await Wishlist.findOne({ client: clientId});
         if(!w)
             return res.status(404).json({ message: "Lista de Desejos não encontrada"});
         w.products = w.products.filter (
@@ -62,7 +65,7 @@ export const clearWishlist = async (req, res) => {
   try {
     const { clientId } = req.params;
 
-    const wishlist = await WishList.findOneAndUpdate(
+    const wishlist = await Wishlist.findOneAndUpdate(
       { client: clientId },
       { products: [] },
       { new: true }
